3ea9878b81a71d60d593c41cb0ded24a3c15d351
Home <sanmhyo@gmail.com>
2025-11-18 00:07:17 +0900

    게임 기반 입력

---
+++ play_mode.py
@@ -0,0 +1,71 @@
+import random
+from pico2d import *
+
+import game_framework
+import game_world
+
+from boy import Boy
+from grass import Grass
+from ball import Ball
+from zombie import Zombie
+
+boy = None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+def init():
+    global boy
+    global balls
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+    game_world.add_collision_pair('grass:ball', grass, None)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+    balls = [Ball(random.randint(100, 1600 - 100), 60, 0) for _ in range(30)]
+    game_world.add_objects(balls, 1)
+
+    #소년과 볼 사이에 대한 충돌검사가 필요하다는 정보를 추가
+    game_world.add_collision_pair('boy:ball', boy, None)
+    for ball in balls:
+        game_world.add_collision_pair('boy:ball', None, ball)
+
+    zombies = [Zombie() for _ in range(4)]
+    game_world.add_objects(zombies, 1)
+
+    for zombie in zombies:
+        game_world.add_collision_pair('zombie:ball', zombie, None)
+
+    game_world.add_collision_pair('zombie:boy', None, boy)
+    for zombie in zombies:
+        game_world.add_collision_pair('zombie:boy', zombie, None)
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
