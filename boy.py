5371e854e292da9f0a408c40c3e763b01bd402ad
Home <sanmhyo@gmail.com>
2025-11-18 00:18:53 +0900

    walk frame별 출력

--- boy.py
+++ boy.py
@@ -5,12 +5,16 @@ class Boy:
         self.x = 800
         self.y = 300
         self.image = load_image('character_Walk.png')
+        self.frame = 0
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        # frame은 인덱스(0..7) 이므로, 실제 픽셀 좌표로 변환해서 전달해야 합니다.
+        self.image.clip_draw(self.frame * 128, 0, 128, 128, self.x, self.y)
 
     def update(self):
-        pass
+        self.frame = (self.frame + 1) % 8
+        delay(0.2)
+
     def get_bb(self):
         return self.x - 25, self.y - 50, self.x + 25, self.y + 50
     def handle_collision(self, group, other):
