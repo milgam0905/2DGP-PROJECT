470521a09c312b86a457fcbc57ac446cc2fac2f5
Home <sanmhyo@gmail.com>
2025-11-18 00:06:43 +0900

    main 기초

--- character.py
+++ character.py
@@ -4,7 +4,7 @@ class Character:
     def __init__(self):
         self.x = 800
         self.y = 300
-        self.image = load_image('character.png')
+        self.image = load_image('character_Walk.png')
 
     def draw(self):
         self.image.draw(self.x, self.y)
